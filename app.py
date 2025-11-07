c6b1cae4daad9262d05f453a34a0a9805ec7bdea
juanecheverri2710 <jfernandoemontoya@gmail.com>
2025-11-07 11:56:53 -0500

    Update UI improvements and fixes

--- app.py
+++ app.py
@@ -162,7 +162,6 @@ def obtenerresultadossuperastromejoradotipoloteria(fechainicio, tipoloteria, max
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
     "Content-Type": "application/x-www-form-urlencoded",
     "Referer": "https://superastro.com.co"
-}
         }
         if tipoloteria.lower() == "sol":
             payload = {
